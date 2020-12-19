2a1a7f2a869daada5a2070e2943fb04be3cbc2ce
AKKing <46888638+millionakking@users.noreply.github.com>
2020-12-19 17:33:44 +0800

    实现优先队列

---
+++ 961数据结构/Queue/PriorityQueue961.cs
@@ -0,0 +1,28 @@
+﻿using _961数据结构.Tree;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _961数据结构.Queue
+{
+    public class PriorityQueue961<T> where T : IComparable
+    {
+        Heap<T> heap = new Heap<T>();
+        public int Size { get { return heap.Count(); } }
+        public bool isEmpty { get { return heap.Count() == 0; } }
+        public T getFont()
+        {
+            return heap.top();
+        }
+
+        public void enqueue(T val)
+        {
+            heap.insert(val);
+        }
+
+        public T dequeue()
+        {
+            return heap.pop();
+        }
+    }
+}
